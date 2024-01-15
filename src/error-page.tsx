9c03e71f02232da3f200eea866a300381efbe770
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-15 09:27:20 -0300

    fix: Correction of typing for the error page.

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,20 +1,36 @@
 import { useRouteError } from "react-router-dom";
 
+interface ErrorResponse {
+  status: number;
+  statusText: string;
+  message: string;
+  internal: boolean;
+  data: string;
+  error: Error;
+}
+
 export default function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as ErrorResponse;
+
+  if (!error) {
+    return (
+      <div className="flex flex-col gap-10 justify-center items-center h-screen w-screen">
+        <h1 className="font-bold text-4xl">Oops!</h1>
+        <p className="text-gray-500 text-xl">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="text-xl">Unknown error occurred.</p>
+      </div>
+    );
+  }
 
   return (
-    <div
-      id="error-page"
-      className="flex flex-col gap-10 justify-center items-center h-screen w-screen"
-    >
+    <div className="flex flex-col gap-10 justify-center items-center h-screen w-screen">
       <h1 className="font-bold text-4xl">Oops!</h1>
       <p className="text-gray-500 text-xl">
         Sorry, an unexpected error has occurred.
       </p>
-      <p>
-        <i className="text-xl">{error.statusText || error.message}</i>
-      </p>
+      <p className="text-xl">{error.statusText || error.message}</p>
     </div>
   );
 }
