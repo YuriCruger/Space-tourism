6f6502c1574db02ebfa7d29506930caae0fad806
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-16 09:24:24 -0300

    feat: create SrcImages.ts to centralize image sources for destination, crew and technology page

---
+++ src/constants/SrcImages.ts
@@ -0,0 +1,36 @@
+//destination page
+import Moon from "assets/destination/image-moon.png";
+import Mars from "assets/destination/image-mars.png";
+import Europa from "assets/destination/image-europa.png";
+import Titan from "assets/destination/image-titan.png";
+
+//crew page
+import CrewGlover from "assets/crew/image-victor-glover.png";
+import CrewAnsari from "assets/crew/image-anousheh-ansari.png";
+import CrewHurley from "assets/crew/image-douglas-hurley.png";
+import CrewMark from "assets/crew/image-mark-shuttleworth.png";
+
+//technology page
+import TechLaunch from "assets/technology/image-launch-vehicle-portrait.jpg";
+import TechSpaceport from "assets/technology/image-spaceport-portrait.jpg";
+import TechSpaceCapsule from "assets/technology/image-space-capsule-portrait.jpg";
+
+export const IMAGE_DESTINATION_SRC = {
+  moon: Moon,
+  mars: Mars,
+  europa: Europa,
+  titan: Titan,
+};
+
+export const IMAGE_CREW_SRC = {
+  douglasHurley: CrewHurley,
+  markShuttleworth: CrewMark,
+  victorGlover: CrewGlover,
+  anoushehAnsari: CrewAnsari,
+};
+
+export const IMAGE_TECHNOLOGY_SRC = {
+  launchVehicle: TechLaunch,
+  spaceport: TechSpaceport,
+  spaceCapsule: TechSpaceCapsule,
+};
